5dce18eaaa260cf9c267511349d91e4cfbbf4a30
Rafael Santos <40042652+rafaelsnv@users.noreply.github.com>
2020-12-06 01:22:56 -0300

    Fix: resultado não encontrado

--- js/appPesquisa.js
+++ js/appPesquisa.js
@@ -3,30 +3,34 @@ const API_KEY = 'fcc5cc127ca513d182be531576591b32';
 // Pesquisar por filmes em index.html ou pesquisa.html
 let pagina = 1;
 
-function exibePesquisa () {
+function exibePesquisa() {
     let sectionConteudoPesquisa = document.getElementById('conteudoPesquisa');
     let texto = "";
 
     // Montar texto HTML das noticias
-    let dados = JSON.parse (this.responseText);
+    let dados = JSON.parse(this.responseText);
 
-    for (i=0; i < dados.results.length; i++) {
-        let filme = dados.results[i];
-        let data = new Date (filme.release_date);
+    if (dados.results.length == 0) {
+        texto = "Nada encontrado";
+    }
+    else {
+        for (i = 0; i < dados.results.length; i++) {
+            let filme = dados.results[i];
+            let data = new Date(filme.release_date);
 
-        if (filme.overview == ""){
-            filme.overview = `Indisponível ou somente em inglês.`
-        }
+            if (filme.overview == "") {
+                filme.overview = `Indisponível ou somente em inglês.`
+            }
 
-        let imagem;
-        if (filme.poster_path == null){
-            imagem = `<img src="/img/imagemNaoDisponivel.png" alt="não disponível">`;
-        }
-        else{
-            imagem = `<img src="https://image.tmdb.org/t/p/w300/${filme.poster_path}" alt="filme01">`;
-        }
+            let imagem;
+            if (filme.poster_path == null) {
+                imagem = `<img src="/img/imagemNaoDisponivel.png" alt="não disponível">`;
+            }
+            else {
+                imagem = `<img src="https://image.tmdb.org/t/p/w300/${filme.poster_path}" alt="filme01">`;
+            }
 
-        texto = texto + `
+            texto = texto + `
         <div class="px-1">
         <a href="https://www.themoviedb.org/movie/${filme.id}">${imagem}</a>
           <p class="py-2"><b class="h4">Filme: </b> <a href="https://www.themoviedb.org/movie/${filme.id}">${filme.title}</p></a>
@@ -35,41 +39,42 @@ function exibePesquisa () {
           <a href="https://www.themoviedb.org/movie/${filme.id}" class="btn btn-secondary text-dark m-2 btn-sm">Ver mais...</a></p>
         </div>
         `;
-    };
-    
+        };
+    }
+
     // Preencher a DIV com o texto HTML
     sectionConteudoPesquisa.innerHTML = texto;
 }
 
-function pesquisarFilmes () {
+function pesquisarFilmes() {
     let query = document.getElementById('txtPesquisa').value;
 
-    let xhr = new XMLHttpRequest ();
+    let xhr = new XMLHttpRequest();
     xhr.onload = exibePesquisa;
-    xhr.open ('GET', `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=pt-BR&query=${query}&page=${pagina}&include_adult=false`);
-    xhr.send ();
+    xhr.open('GET', `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=pt-BR&query=${query}&page=${pagina}&include_adult=false`);
+    xhr.send();
 }
 
-function exibeMaisPesquisa () {
+function exibeMaisPesquisa() {
     let sectionConteudoPesquisa = document.getElementById('conteudoPesquisa');
     let texto = sectionConteudoPesquisa.innerHTML;
 
     // Montar texto HTML das noticias
-    let dados = JSON.parse (this.responseText);
+    let dados = JSON.parse(this.responseText);
 
-    for (i=0; i < dados.results.length; i++) {
+    for (i = 0; i < dados.results.length; i++) {
         let filme = dados.results[i];
-        let data = new Date (filme.release_date);
+        let data = new Date(filme.release_date);
 
-        if (filme.overview == ""){
+        if (filme.overview == "") {
             filme.overview = `Indisponível ou somente em inglês.`
         }
 
         let imagem;
-        if (filme.poster_path == null){
+        if (filme.poster_path == null) {
             imagem = `<img src="/img/imagemNaoDisponivel.png" alt="não disponível">`;
         }
-        else{
+        else {
             imagem = `<img src="https://image.tmdb.org/t/p/w300/${filme.poster_path}" alt="filme01">`;
         }
 
@@ -83,22 +88,22 @@ function exibeMaisPesquisa () {
         </div>
         `;
     };
-    
+
     // Preencher a DIV com o texto HTML
     sectionConteudoPesquisa.innerHTML = texto;
     pagina++;
 }
 
-function pesquisarMaisFilmes () {
+function pesquisarMaisFilmes() {
     let query = document.getElementById('txtPesquisa').value;
     pagina++;
 
-    let xhr = new XMLHttpRequest ();
+    let xhr = new XMLHttpRequest();
     xhr.onload = exibeMaisPesquisa;
-    xhr.open ('GET', `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=pt-BR&query=${query}&page=${pagina}&include_adult=false`);
-    xhr.send ();
+    xhr.open('GET', `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=pt-BR&query=${query}&page=${pagina}&include_adult=false`);
+    xhr.send();
 }
 
-document.getElementById ('btnPesquisa').addEventListener ('click', pesquisarFilmes);
+document.getElementById('btnPesquisa').addEventListener('click', pesquisarFilmes);
 
-document.getElementById ('btnMaisPesquisa').addEventListener ('click', pesquisarMaisFilmes);
+document.getElementById('btnMaisPesquisa').addEventListener('click', pesquisarMaisFilmes);
